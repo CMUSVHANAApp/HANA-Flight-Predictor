d46547f3176e4e9984c6689084017bf968bfb32e
Edward Akoto <edward.akoto@sv.cmu.edu>
2012-12-06 10:27:17 -0800

    Initial Commit

---
+++ FlightAppV2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlightAppV2
+//
+//  Created by Edward Akoto on 12/6/12.
+//  Copyright (c) 2012 Edward Akoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlightAppV2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlightAppV2
+//
+//  Created by Edward Akoto on 12/6/12.
+//  Copyright (c) 2012 Edward Akoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
