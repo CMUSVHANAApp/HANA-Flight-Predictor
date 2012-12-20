d04bf6092da513b84aa00c7c7e5ecf9bdb593544
Edward Akoto <edward.akoto@sv.cmu.edu>
2012-12-20 15:22:33 -0800

    merged with master

--- FlightAppV2/AppDelegate.m
+++ FlightAppV2/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    sleep(5);
+   sleep(5);
     
     // Override point for customization after application launch.
     return YES;
